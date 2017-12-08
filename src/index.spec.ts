1015cc1fbdb4893e05632b79932a793c485bc761
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-07 23:46:14 -0200

    adding editorconfig and replacing indentation all around

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,64 +2,64 @@ import * as chai from 'chai';
 import jsMasker from './index';
 
 describe('Masker', () => {
-    it('should be able to import jsMasker', function() {
-        chai.expect(jsMasker).to.be.not.null;
-        chai.expect(jsMasker).to.be.not.undefined;
-    });
+  it('should be able to import jsMasker', function () {
+    chai.expect(jsMasker).to.be.not.null;
+    chai.expect(jsMasker).to.be.not.undefined;
+  });
 
-    it('should be a function', function() {
-        // testing if .bind exists anywhere else
-        const emptyObject: any = {};
-        const hello: any = 'hello';
-        chai.assert.isUndefined(emptyObject.bind);
-        chai.assert.isUndefined(hello.bind);
+  it('should be a function', function () {
+    // testing if .bind exists anywhere else
+    const emptyObject: any = {};
+    const hello: any = 'hello';
+    chai.assert.isUndefined(emptyObject.bind);
+    chai.assert.isUndefined(hello.bind);
 
-        // now in our function
-        chai.assert.isDefined(jsMasker.bind);
-    });
+    // now in our function
+    chai.assert.isDefined(jsMasker.bind);
+  });
 
-    it('should be able to handle empty masks', () => {
-        chai.expect(jsMasker('99', '1')).to.equal('1');
-        chai.expect(jsMasker('99', '22')).to.equal('22');
-        chai.expect(jsMasker('99', '333')).to.equal('33');
-        chai.expect(jsMasker('99', '4444')).to.equal('44');
-    });
+  it('should be able to handle empty masks', () => {
+    chai.expect(jsMasker('99', '1')).to.equal('1');
+    chai.expect(jsMasker('99', '22')).to.equal('22');
+    chai.expect(jsMasker('99', '333')).to.equal('33');
+    chai.expect(jsMasker('99', '4444')).to.equal('44');
+  });
 
-    it('should be able to apply simple masks', function() {
-        chai.expect(jsMasker('(99)', '88')).to.equal('(88');
-        chai.expect(jsMasker('(99)', '77')).to.equal('(77');
-        chai.expect(jsMasker('-99-', '77')).to.equal('-77');
-    });
+  it('should be able to apply simple masks', function () {
+    chai.expect(jsMasker('(99)', '88')).to.equal('(88');
+    chai.expect(jsMasker('(99)', '77')).to.equal('(77');
+    chai.expect(jsMasker('-99-', '77')).to.equal('-77');
+  });
 
-    it('should be able to apply more complex masks', function() {
-        chai.expect(jsMasker('(99) 9999', '777777')).to.equal('(77) 7777');
-        chai.expect(jsMasker('(99) 9999-9999', '1234567890')).to.equal('(12) 3456-7890');
-        chai.expect(jsMasker('(99) 99999-9999', '12345678901')).to.equal('(12) 34567-8901');
-    });
+  it('should be able to apply more complex masks', function () {
+    chai.expect(jsMasker('(99) 9999', '777777')).to.equal('(77) 7777');
+    chai.expect(jsMasker('(99) 9999-9999', '1234567890')).to.equal('(12) 3456-7890');
+    chai.expect(jsMasker('(99) 99999-9999', '12345678901')).to.equal('(12) 34567-8901');
+  });
 
-    it('should be able to handle optional chars', function() {
-        chai.expect(jsMasker('?999', '12')).to.equal('12');
-        chai.expect(jsMasker('?999', '12345')).to.equal('123');
+  it('should be able to handle optional chars', function () {
+    chai.expect(jsMasker('?999', '12')).to.equal('12');
+    chai.expect(jsMasker('?999', '12345')).to.equal('123');
 
-        chai.expect(jsMasker('(99) 9999?9-9999', '1234567890')).to.equal('(12) 3456-7890');
-        chai.expect(jsMasker('(99) 9?9999-9999', '1234567890')).to.equal('(12) 3456-7890');
-        chai.expect(jsMasker('(99) 9999?9-9999', '12345678901')).to.equal('(12) 34567-8901');
-        chai.expect(jsMasker('(99) 9999?9-99999', '123456789012')).to.equal('(12) 34567-89012');
+    chai.expect(jsMasker('(99) 9999?9-9999', '1234567890')).to.equal('(12) 3456-7890');
+    chai.expect(jsMasker('(99) 9?9999-9999', '1234567890')).to.equal('(12) 3456-7890');
+    chai.expect(jsMasker('(99) 9999?9-9999', '12345678901')).to.equal('(12) 34567-8901');
+    chai.expect(jsMasker('(99) 9999?9-99999', '123456789012')).to.equal('(12) 34567-89012');
 
-        chai.expect(jsMasker('(99) 9999-999?9', '001234567')).to.equal('(00) 1234-567');
-        chai.expect(jsMasker('(99) 9999-999?9', '0012345678')).to.equal('(00) 1234-5678');
-        chai.expect(jsMasker('(99) 9999-9999?9', '001234567890')).to.equal('(00) 1234-56789');
-    });
+    chai.expect(jsMasker('(99) 9999-999?9', '001234567')).to.equal('(00) 1234-567');
+    chai.expect(jsMasker('(99) 9999-999?9', '0012345678')).to.equal('(00) 1234-5678');
+    chai.expect(jsMasker('(99) 9999-9999?9', '001234567890')).to.equal('(00) 1234-56789');
+  });
 
-    it('should be able to multiple optional chars', function() {
-        chai.expect(jsMasker('(99) ?999?99-9999?9', '001234567890')).to.equal('(00) 12345-67890');
-        chai.expect(jsMasker('(99) ?999?99-9999?9', '00123456789')).to.equal('(00) 1234-56789');
-        chai.expect(jsMasker('(99) 9?99?99-99?999', '0012345678')).to.equal('(00) 123-45678');
-    });
+  it('should be able to multiple optional chars', function () {
+    chai.expect(jsMasker('(99) ?999?99-9999?9', '001234567890')).to.equal('(00) 12345-67890');
+    chai.expect(jsMasker('(99) ?999?99-9999?9', '00123456789')).to.equal('(00) 1234-56789');
+    chai.expect(jsMasker('(99) 9?99?99-99?999', '0012345678')).to.equal('(00) 123-45678');
+  });
 
-    it('should ignore non digits', function () {
-        chai.expect(jsMasker('(99)9?99?99-99?999', '0A12345678')).to.equal('(01)234-5678');
-        chai.expect(jsMasker('(99)9?99?99-99?999', 'AAA')).to.equal('');
-        chai.expect(jsMasker('(99)9?99?99-99?999', 'AA1A')).to.equal('(1');
-    });
+  it('should ignore non digits', function () {
+    chai.expect(jsMasker('(99)9?99?99-99?999', '0A12345678')).to.equal('(01)234-5678');
+    chai.expect(jsMasker('(99)9?99?99-99?999', 'AAA')).to.equal('');
+    chai.expect(jsMasker('(99)9?99?99-99?999', 'AA1A')).to.equal('(1');
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,57 +4,57 @@ const DIGIT = '9';
 const OPTIONAL = '?';
 
 function jsMasker(mask: string, value: string): string {
-    const inputArray = value.split('') || [];
-    const digitsEntered = inputArray
-        .map((char, index) => (isNumber(char) ? 1 : 0))
-        .reduce((prev, next) => (prev + next), 0);
-    if (digitsEntered == 0) return '';
+  const inputArray = value.split('') || [];
+  const digitsEntered = inputArray
+    .map((char, index) => (isNumber(char) ? 1 : 0))
+    .reduce((prev, next) => (prev + next), 0);
+  if (digitsEntered == 0) return '';
 
-    const inputLength = value.length;
-    const maskArray = mask.split('') || [];
-    const indexes = maskArray.map((char, index) => (char === OPTIONAL ? index : '')).filter(String);
+  const inputLength = value.length;
+  const maskArray = mask.split('') || [];
+  const indexes = maskArray.map((char, index) => (char === OPTIONAL ? index : '')).filter(String);
 
-    // counts 9s in the mask
-    const digitPlaces = maskArray
-        .map((char, index) => (char === DIGIT ? 1 : 0))
-        .reduce((prev, next) => (prev + next), 0);
+  // counts 9s in the mask
+  const digitPlaces = maskArray
+    .map((char, index) => (char === DIGIT ? 1 : 0))
+    .reduce((prev, next) => (prev + next), 0);
 
-    const maskedValue = maskArray;
+  const maskedValue = maskArray;
 
-    let nextElement = 0;
-    let jump = false;
-    let lengthDiff = digitPlaces - inputLength;
-    maskArray.map((char, index) => {
-        if (jump) {
-            lengthDiff--;
-            jump = false;
-            maskedValue[index] = '';
-            return;
-        }
-        if (char === OPTIONAL) {
-            jump = lengthDiff > 0;
-            maskedValue[index] = '';
-            return;
-        }
-        if (char === DIGIT && inputLength > nextElement) {
-            const isNum = isNumber(value[nextElement]);
-            if (isNum) {
-                maskedValue[index] = value[nextElement];
-                nextElement++;
-                return;
-            }
-            while (inputLength > nextElement && !isNumber(value[nextElement])) {
-                nextElement++;
-            }
-            maskedValue[index] = isNumber(value[nextElement]) ? value[nextElement] : '';
-            nextElement++;
-        } else if (inputLength <= nextElement) {
-            maskedValue[index] = '';
-        }
-    });
-    return maskedValue.join("");
+  let nextElement = 0;
+  let jump = false;
+  let lengthDiff = digitPlaces - inputLength;
+  maskArray.map((char, index) => {
+    if (jump) {
+      lengthDiff--;
+      jump = false;
+      maskedValue[index] = '';
+      return;
+    }
+    if (char === OPTIONAL) {
+      jump = lengthDiff > 0;
+      maskedValue[index] = '';
+      return;
+    }
+    if (char === DIGIT && inputLength > nextElement) {
+      const isNum = isNumber(value[nextElement]);
+      if (isNum) {
+        maskedValue[index] = value[nextElement];
+        nextElement++;
+        return;
+      }
+      while (inputLength > nextElement && !isNumber(value[nextElement])) {
+        nextElement++;
+      }
+      maskedValue[index] = isNumber(value[nextElement]) ? value[nextElement] : '';
+      nextElement++;
+    } else if (inputLength <= nextElement) {
+      maskedValue[index] = '';
+    }
+  });
+  return maskedValue.join("");
 } // preceeded by optional
 
 function isNumber(value) {
-    return value && value.match(/[0-9]/) != null;
+  return value && value.match(/[0-9]/) != null;
 }
