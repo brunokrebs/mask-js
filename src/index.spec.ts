1c328d710736f6315bce0c658c1bff07312c8757
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-07 23:10:55 -0200

    small library to apply masks to numbers

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -18,7 +18,39 @@ describe('Masker', () => {
         chai.assert.isDefined(jsMasker.bind);
     });
 
-    it('should be able to apply a simple mask', function() {
-        chai.expect(jsMasker('(99)', '88')).to.equal('(88)');
+    it('should be able to handle empty masks', () => {
+        chai.expect(jsMasker('99', '1')).to.equal('1');
+        chai.expect(jsMasker('99', '22')).to.equal('22');
+        chai.expect(jsMasker('99', '333')).to.equal('33');
+        chai.expect(jsMasker('99', '4444')).to.equal('44');
+    });
+
+    it('should be able to apply simple masks', function() {
+        chai.expect(jsMasker('(99)', '88')).to.equal('(88');
+        chai.expect(jsMasker('(99)', '77')).to.equal('(77');
+        chai.expect(jsMasker('-99-', '77')).to.equal('-77');
+    });
+
+    it('should be able to apply more complex masks', function() {
+        chai.expect(jsMasker('(99) 9999', '777777')).to.equal('(77) 7777');
+        chai.expect(jsMasker('(99) 9999-9999', '1234567890')).to.equal('(12) 3456-7890');
+        chai.expect(jsMasker('(99) 99999-9999', '12345678901')).to.equal('(12) 34567-8901');
+    });
+
+    it('should be able to handle optional chars', function() {
+        chai.expect(jsMasker('(99) 9999?9-9999', '1234567890')).to.equal('(12) 3456-7890');
+        chai.expect(jsMasker('(99) 9?9999-9999', '1234567890')).to.equal('(12) 3456-7890');
+        chai.expect(jsMasker('(99) 9999?9-9999', '12345678901')).to.equal('(12) 34567-8901');
+        chai.expect(jsMasker('(99) 9999?9-99999', '123456789012')).to.equal('(12) 34567-89012');
+
+        chai.expect(jsMasker('(99) 9999-999?9', '001234567')).to.equal('(00) 1234-567');
+        chai.expect(jsMasker('(99) 9999-999?9', '0012345678')).to.equal('(00) 1234-5678');
+        chai.expect(jsMasker('(99) 9999-9999?9', '001234567890')).to.equal('(00) 1234-56789');
+    });
+
+    it('should be able to multiple optional chars', function() {
+        chai.expect(jsMasker('(99) ?999?99-9999?9', '001234567890')).to.equal('(00) 12345-67890');
+        chai.expect(jsMasker('(99) ?999?99-9999?9', '00123456789')).to.equal('(00) 1234-56789');
+        chai.expect(jsMasker('(99) 9?99?99-99?999', '0012345678')).to.equal('(00) 123-45678');
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
 export default jsMasker;
 
 const DIGIT = '9';
-const ALPHA = 'A';
-const ALPHANUM = 'S';
+const OPTIONAL = '?';
 
 function jsMasker(mask: string, value: string): string {
-    const availableSpaces = mask.replace(/\W/g, '');
-    const completeInput = availableSpaces.length === value.length;
-    const maskArray = mask.split('');
+    const inputLength = value.length;
+    const maskArray = mask.split('') || [];
+    const indexes = maskArray.map((char, index) => (char === OPTIONAL ? index : '')).filter(String);
+
+    // counts 9s in the mask
+    const digitPlaces = maskArray
+        .map((char, index) => (char === DIGIT ? 1 : 0))
+        .reduce((prev, next) => (prev + next), 0);
+    const maskLength = maskArray.length - indexes.length;
     const maskedValue = maskArray;
+
     let nextElement = 0;
+    let jump = false;
+    let lengthDiff = digitPlaces - inputLength;
     maskArray.map((char, index) => {
-        if (char === DIGIT && value[nextElement].match(/[0-9]/)) {
+        if (jump) {
+            lengthDiff--;
+            jump = false;
+            maskedValue[index] = '';
+            return;
+        }
+        if (char === OPTIONAL) {
+            jump = lengthDiff > 0;
+            maskedValue[index] = '';
+            return;
+        }
+        if (char === DIGIT && inputLength > nextElement && isNumber(value[nextElement])) {
             maskedValue[index] = value[nextElement];
             nextElement++;
+        } else if (inputLength <= nextElement) {
+            maskedValue[index] = '';
         }
     });
     return maskedValue.join("");
+} // preceeded by optional
+
+function isNumber(value) {
+    return value.match(/[0-9]/);
 }
